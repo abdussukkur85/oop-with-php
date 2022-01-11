53ea3a6cc6b09537c6c451f7f8d9f2c60027830a
abdus-sukkur <abdussukkur73@gmail.com>
2022-01-11 08:32:57 +0600

    Initial Commit and create class and object

---
+++ 1)ClassAndObject.php
@@ -0,0 +1,13 @@
+<?php
+/*
+    Class: A blueprint of and Object
+    Object: Object is a data type
+    Access modifier: public, private, protected
+*/
+class Transaction { // creating a class
+    private $amount; // property
+    protected $name;
+    public $description;
+}
+
+$transaction = new Transaction; // Object
